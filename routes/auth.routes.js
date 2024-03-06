6a2bd6bf875eaae19547b6ea870a9b3420f39ebf
ankit010IITJ <ankitraj69793@gmail.com>
2024-03-06 23:04:24 +0530

    Added middleware for authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,12 @@
 /**
- * POST localhost:8888/
+ * POST localhost:8888/cycle_sharing/api/v1/auth/signup
+ * 
+ * I need to intercept this
  */
 
 const authController = require("../controllers/auth.controller")
+const authMW = require("../middlewares/auth.mw")
 
 module.exports = (app) => {
-    app.post("/cycle_sharing/api/v1/auth/signup", authController.signup)
+    app.post("/cycle_sharing/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
 }
