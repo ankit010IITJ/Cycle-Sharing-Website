381e7bdedd4fc5fb49afed1910121e31f1a5b999
ankit010IITJ <ankitraj69793@gmail.com>
2024-03-07 01:20:29 +0530

    Updated routes with signin functionalities

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,4 +9,10 @@ const authMW = require("../middlewares/auth.mw")
 
 module.exports = (app) => {
     app.post("/cycle_sharing/api/v1/auth/signup", [authMW.verifySignUpBody], authController.signup)
+
+    /**
+     * route for
+     * POST localhost:8888/cycle_sharing/api/v1/auth/signin
+     */
+    app.post("cycle_sharing/api/v1/auth/signin", authController.signin)
 }
